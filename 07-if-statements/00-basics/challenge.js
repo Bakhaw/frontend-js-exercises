7e869d35d44432504568c71644e065f2544793e9
Faïssal <faissal.hattou@gmail.com>
2017-07-06 16:49:15 +0200

    07 90% done

--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -1,5 +1,17 @@
-module.exports.favoriteNumber = undefined;
+module.exports.favoriteNumber = function(fav, guess) {
+  if (guess > fav) {return "Too high"}
+  else if (fav > guess) {return "Too low"}
+  else {return "You got it!"}
+};
 
-module.exports.checkLock = undefined;
+module.exports.checkLock = function(a,b,c,d) {
+  if (a != 3 && a != 5 && a != 7) {return "incorrect"}
+  else if (b != 2) {return "incorrect"}
+  else if (c < 5 || c > 100)  {return "incorrect"}
+  else if (d >= 9 && d <= 20) {return "incorrect"}
+  else {return "correct"}
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function() {
+
+};
